53d559f8803e56a0f9b3dc6029b28482c31544ae
Jackson Abascal <jacksonabascal@gmail.com>
2018-10-25 21:23:17 -0400

    Update Z_algo.cpp

--- string/Z_algo.cpp
+++ string/Z_algo.cpp
@@ -1,14 +1,16 @@
-//v[i] is longest prefix of s starting from i
-void z_algo(const string &s, vector<int> &Z) {
+//v[i] is LCP(s, suffix of s starting at i)
+vector<int> z_algo(const string &s) {
 	int n = s.size(), l = 0, r = 0;
-	Z.resize(n);
+	vector<int> Z(n);
 	for(int i = 1; i < n; ++i) {
 		if(i <= r && Z[i-l] < r-i+1)
 			Z[i] = Z[i-l];
 		else {
 			l = i, r = max(r,i);
-			while(r < n & s[r-l] == s[r]) ++r;
+			while(r < n && s[r-l] == s[r]) ++r;
 			Z[i] = r---l;
 		}
 	}
+	Z[0] = n;
+	return Z;
 }
