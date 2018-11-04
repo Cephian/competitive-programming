5b91208d8a9f416847f7e7ece5a7acbdaccd7922
cephian <jacksonabascal@gmail.com>
2018-11-04 00:29:43 -0400

    updated rmq

--- data structures/static rmq/max_value.cpp
+++ data structures/static rmq/max_value.cpp
@@ -1,7 +1,7 @@
 // O(n log n) space, O(1) query RMQ
 struct rMq {
 	vector<vector<int> > t;
-	rmq(){}
+	rMq(){}
 	void init(int* a, int n) {
 		t.resize(32-__builtin_clz(n),vector<int>(n));
 		for(int i = 0; i < n; ++i)
@@ -10,7 +10,7 @@ struct rMq {
 			for(int i = 0; i < n; ++i)
 				t[k][i] = (i+p<n)?max(t[k-1][i],t[k-1][i+p]):t[k-1][i];
 	}
-	rmq(int* a, int n) {init(a,n);}
+	rMq(int* a, int n) {init(a,n);}
 	//inclusive max query
 	inline int query(int l, int r) const {
 		int p = 31-__builtin_clz(r-l+1);
