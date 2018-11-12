765318ba07d3ad47ec3a031d726370d209072b26
Jackson Abascal <jacksonabascal@gmail.com>
2018-11-12 12:16:33 -0500

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
-#define fi first
-#define se second
+#define print(x) cerr << #x << " = " << x << endl;
 using namespace std;
-typedef pair<int,int> pii;
 typedef long long ll;
 
 
 
 int main() {	
-	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	ios::sync_with_stdio(0), cin.tie(0);
 
 
 }
